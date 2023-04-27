d3ba866607f35588536732f127d257cbd89207b5
Андрейка <2449>
2023-04-27 12:37:46 +0300

    Начало великого

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import Post
+# Create your views here.
+
+
+class Bloglist(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class AboutPageView(TemplateView):
+    templates_name = 'about.html'
+
